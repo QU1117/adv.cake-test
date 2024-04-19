ccb2b41b2ea858c3d1e0e38d2b51c1eef3a12364
Dimitry Boyko <n30nv4mp@yandex.ru>
2024-04-20 02:14:29 +0300

    Изменение функции reverseString для работы с отличными от английского символами.

--- src/AdvCake/Classes/StringReverser.php
+++ src/AdvCake/Classes/StringReverser.php
@@ -12,22 +12,41 @@ class StringReverser implements StringReverserInterface
 {
     public function reverseString(string $sentence): string
     {
-        $sizeRegistry = [];
+        $caseRegistry = [];
 
-        for ($i = 0; $i < strlen($sentence); $i++) {
-            if ($sentence[$i] === strtoupper($sentence[$i])) {
-                $sizeRegistry[$i] = true;
-            } else $sizeRegistry[$i] = false;
+        for ($i = 0; $i < mb_strlen($sentence, mb_internal_encoding()); $i++) {
+            $char = mb_substr($sentence, $i, 1, mb_internal_encoding());
+
+            if (mb_strtolower($char, mb_internal_encoding()) === $char) {
+                $caseRegistry[$i] = false;
+            } else if (mb_strtoupper($char, mb_internal_encoding()) === $char) {
+                $caseRegistry[$i] = true;
+            }
         }
 
-        $reversedString = strrev($sentence);
+        $chars = mb_str_split($sentence, 1, mb_internal_encoding());
+        $reversedString = implode('', array_reverse($chars));
 
-        for ($i = 0; $i < strlen($reversedString); $i++) {
-            if ($sizeRegistry[$i]) {
-                $reversedString[$i] = strtoupper($reversedString[$i]);
-            } else $reversedString[$i] = strtolower($reversedString[$i]);
+        $result = '';
+
+        for ($i = 0;
+             $i < mb_strlen($reversedString, mb_internal_encoding());
+             $i++)
+        {
+            $char = mb_substr($reversedString, $i, 1, mb_internal_encoding());
+
+            if ($caseRegistry[$i]) {
+                $result .= mb_convert_case(
+                    $char,
+                    MB_CASE_UPPER,
+                    mb_internal_encoding()
+                );
+            } else $result .= mb_convert_case(
+                $char,
+                MB_CASE_LOWER,
+                mb_internal_encoding());
         }
 
-        return $reversedString;
+        return $result;
     }
 }

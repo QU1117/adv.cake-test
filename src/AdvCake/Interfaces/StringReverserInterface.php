4c8afcb9d03b048a865f18de17e35457b135ed87
Dimitry Boyko <n30nv4mp@yandex.ru>
2024-04-20 01:49:59 +0300

    исправлена опечатка в интерфейсе StringReverserInterface

--- src/AdvCake/Interfaces/StringReverserInterface.php
+++ src/AdvCake/Interfaces/StringReverserInterface.php
@@ -11,7 +11,7 @@ interface StringReverserInterface
     /**
      * Изменяет порядок символов в строке на обратный.
      * @param string $sentence Строка, порядок символов которой нужно поменять.
-     * @return string Строка с измененным порядком символом.
+     * @return string Строка с измененным порядком символов.
      */
     public function reverseString(string $sentence): string;
 }

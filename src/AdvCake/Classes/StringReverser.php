b2e1d025cd8c5826f928ed90380db04788f8549a
Dimitry Boyko <n30nv4mp@yandex.ru>
2024-04-19 22:52:42 +0300

    Добавлено небольшое описание класса StringReverser

--- src/AdvCake/Classes/StringReverser.php
+++ src/AdvCake/Classes/StringReverser.php
@@ -4,6 +4,10 @@ namespace AdvCake\Classes;
 
 use AdvCake\Interfaces\StringReverserInterface;
 
+/**
+ * Класс, реализующий функционал, связанный с перестановкой символов в строке в
+ * обратном порядке.
+ */
 class StringReverser implements StringReverserInterface
 {
     public function reverseString(string $sentence): string

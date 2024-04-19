b865aabed9cef85ac9f2c503d7a3c2b168ddc06c
Dimitry Boyko <n30nv4mp@yandex.ru>
2024-04-20 02:22:59 +0300

    Добавлено еще 6 тестов

--- tests/Unit/ReverseStringTest.php
+++ tests/Unit/ReverseStringTest.php
@@ -26,4 +26,70 @@ describe('Тестирование инверсии строк', function () {
                 ->toBe($reversedString);
         }
     );
+
+    test(
+        "Переворачивает строку 'houSe'",
+        function () use ($stringManipulator) {
+            $originalString = 'houSe';
+            $reversedString = 'esuOh';
+
+            expect($stringManipulator->reverseString($originalString))
+                ->toBe($reversedString);
+        }
+    );
+
+    test(
+        "Переворачивает строку 'домИК'",
+        function () use ($stringManipulator) {
+            $originalString = 'домИК';
+            $reversedString = 'кимОД';
+
+            expect($stringManipulator->reverseString($originalString))
+                ->toBe($reversedString);
+        }
+    );
+
+    test(
+        "Переворачивает строку 'elEpHant'",
+        function () use ($stringManipulator) {
+            $originalString = 'elEpHant';
+            $reversedString = 'tnAhPele';
+
+            expect($stringManipulator->reverseString($originalString))
+                ->toBe($reversedString);
+        }
+    );
+
+    test(
+        "Переворачивает строку 'Зима'",
+        function () use ($stringManipulator) {
+            $originalString = 'Зима';
+            $reversedString = 'Амиз';
+
+            expect($stringManipulator->reverseString($originalString))
+                ->toBe($reversedString);
+        }
+    );
+
+    test(
+        "Переворачивает строку \"is 'cold' now\"",
+        function () use ($stringManipulator) {
+            $originalString = "is 'cold' now";
+            $reversedString = "si 'dloc' won";
+
+            expect($stringManipulator->reverseString($originalString))
+                ->toBe($reversedString);
+        }
+    );
+
+    test(
+        "Переворачивает строку 'это «Так» \"просто\"'",
+        function () use ($stringManipulator) {
+            $originalString = 'это «Так» "просто"';
+            $reversedString =  'отэ «Так» "отсорп"';
+
+            expect($stringManipulator->reverseString($originalString))
+                ->toBe($reversedString);
+        }
+    );
 });

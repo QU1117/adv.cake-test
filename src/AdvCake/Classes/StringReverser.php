2231c02e8348b682aed617459495d49127113c25
Dimitry Boyko <n30nv4mp@yandex.ru>
2024-04-19 21:57:33 +0300

    Добавлен класс StringReverser, реализующий интерфейс StringReverserInterface

---
+++ src/AdvCake/Classes/StringReverser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AdvCake\Classes;
+
+use AdvCake\Interfaces\StringReverserInterface;
+
+class StringReverser implements StringReverserInterface
+{
+    public function reverseString(string $sentence): string
+    {
+       // TODO: Implement reverseString() method.
+    }
+}
